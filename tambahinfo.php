b62e5f7d66439b5260d4e7cfccd3d031770032b5
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-01-28 23:37:55 +0700

    Papan Informasi Elektronik

--- tambahinfo.php
+++ tambahinfo.php
@@ -1,12 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tambah Informasi</title>
 </head>
+
 <body>
-    
+    <form action="aksiinfo.php" method="post" enctype="multipart/from-data" name="form1" id="form1">
+        <table border="0" align="center">
+
+        </table>
+    </form>
 </body>
+
 </html>
