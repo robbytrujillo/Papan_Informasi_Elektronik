82926480b6ed4baf1a0cacf93181bc975c32eb76
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-01-29 17:32:45 +0700

    Papan Informasi Elektronik | index & style

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="stylesheet.css">
+    <script type="text/javascript" src="jquery-1.7.2.js"></script>
+    <script type="text/javascript" src="coolclock.js"></script>
+    <script type="text/javascript" src="moreskins.js"></script>
+</head>
+
+<body>
+    <!-- kontainer utama -->
+    <div id="kontainer">
+        <div id="banner">
+            <div id="jam"></div>
+            <div id="logo"></div>
+        </div>
+        <div id="badan">
+            <div id="judul"></div>
+            <div id="isi"></div>
+        </div>
+        <div id="ticker"></div>
+        <div id="jam">
+            <canvas id="clk2" style="display: block;" class="CoolClock:fancy"></canvas>
+        </div>
+        <div id="logo">Papan Pengumuman LOKOMEDIA</div>
+    </div>
+</body>
+
+</html>
