b7e2dded36ecd5afcbba7401e60f379e2276478f
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-01-29 15:10:02 +0700

    Papan Informasi Elektronik | tambah info

--- tambahinfo.php
+++ tambahinfo.php
@@ -11,7 +11,26 @@
 <body>
     <form action="aksiinfo.php" method="post" enctype="multipart/from-data" name="form1" id="form1">
         <table border="0" align="center">
-
+            <tr>
+                <td colspan="2"><strong>Tambah Informasi</strong></td>
+            </tr>
+            <tr>
+                <td>Judul</td>
+                <td><input type="text" name="judul" id="judul" /></td>
+            </tr>
+            <tr>
+                <td valign="top">Isi</td>
+                <td><textarea name="isi" id="isi" cols="45" rows="5" style="width: 100%; height: 300px;"></textarea>
+                </td>
+            </tr>
+            <tr>
+                <td>Lama Durasi</td>
+                <td><input type="text" name="lama" id="lama" /></td>
+            </tr>
+            <tr>
+                <td>&nbsp;</td>
+                <td><input type="submit" name="Simpan" id="Simpan" value="Simpan"></td>
+            </tr>
         </table>
     </form>
 </body>
